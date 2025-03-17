38bd4406b53d544c2816639fc045a54dc431a6ed
JonesAshbur <JonesAshbur@gmail.com>
2025-03-17 10:37:21 +0800

    update

--- ginCase5/controllers/admin/userController.go
+++ ginCase5/controllers/admin/userController.go
@@ -1,6 +1,11 @@
 package admin
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"path"
+)
 
 type UserController struct {
 	BaseController
@@ -12,9 +17,51 @@ func (con *UserController) Index(c *gin.Context) {
 }
 
 func (con *UserController) Add(c *gin.Context) {
-	c.String(200, "add user")
+	//c.String(200, "add user")
+	c.HTML(200, "admin/userAdd.html", gin.H{})
 }
 
 func (con *UserController) Edit(c *gin.Context) {
 	c.String(200, "edit user")
+}
+
+// 单文件上传
+func (con *UserController) DoUpload(c *gin.Context) {
+	username := c.PostForm("username")
+	file, err := c.FormFile("face")
+
+	//获取文件名称file.Filename
+	dst := path.Join("./static/upload", file.Filename)
+	if err == nil {
+		err := c.SaveUploadedFile(file, dst)
+		if err != nil {
+			return
+		}
+	}
+	log.Println(username, file.Filename)
+	c.JSONP(http.StatusOK, gin.H{
+		"success":  true,
+		"username": username,
+		"dst":      dst,
+	})
+}
+
+// 多文件上传
+func (con *UserController) DoMultipleUpload(c *gin.Context) {
+	username := c.PostForm("username")
+
+	form, _ := c.MultipartForm()
+	files, _ := form.File["face[]"]
+
+	for _, file := range files {
+		dst := path.Join("./static/upload", file.Filename)
+		err := c.SaveUploadedFile(file, dst)
+		if err != nil {
+			return
+		}
+	}
+	c.JSONP(http.StatusOK, gin.H{
+		"success":  true,
+		"username": username,
+	})
 }
--- ginCase5/routers/adminRouters.go
+++ ginCase5/routers/adminRouters.go
@@ -15,6 +15,8 @@ func AdminRoutersInit(r *gin.Engine) {
 		adminRouters.GET("/user", (&admin.UserController{}).Index)
 		adminRouters.GET("/user/add", (&admin.UserController{}).Add)
 		adminRouters.GET("/user/edit", (&admin.UserController{}).Edit)
+		adminRouters.POST("/user/doUpload", (&admin.UserController{}).DoUpload)
+		adminRouters.POST("/user/doMultipleUpload", (&admin.UserController{}).DoMultipleUpload)
 		adminRouters.GET("/article", (&admin.ArticleController{}).Index)
 		adminRouters.GET("/article/add", (&admin.ArticleController{}).Add)
 		adminRouters.GET("/article/edit", (&admin.ArticleController{}).Edit)
