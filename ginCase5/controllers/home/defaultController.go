fe8c2190a955d95972bd7176e4bde702bc087918
JonesAshbur <JonesAshbur@gmail.com>
2025-03-17 21:23:29 +0800

    update

--- ginCase5/controllers/home/defaultController.go
+++ ginCase5/controllers/home/defaultController.go
@@ -2,6 +2,7 @@ package home
 
 import (
 	"fmt"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,6 +10,17 @@ import (
 type DefaultController struct{}
 
 func (con *DefaultController) Index(c *gin.Context) {
+	session := sessions.Default(c)
+	//session.Options可以设置session过期时间以及其它参数
+	session.Options(sessions.Options{
+		MaxAge: 3600,
+	})
+	session.Set("username", "alfred")
+	//保存设置的session
+	err := session.Save()
+	if err != nil {
+		return
+	}
 	c.SetCookie("username", "jones", 3600, "/", "localhost", false, false)
 	c.HTML(http.StatusOK, "default/index.html", gin.H{
 		"msg":  "msg信息",
@@ -21,6 +33,9 @@ func (con *DefaultController) DeleteCookie(c *gin.Context) {
 	c.String(http.StatusOK, "删除成功")
 }
 func (con *DefaultController) News(c *gin.Context) {
+	session := sessions.Default(c)
+	username := session.Get("username")
+	fmt.Println("session设置的username：", username)
 	cookie, err := c.Cookie("username")
 	if err != nil {
 		return
--- ginCase5/mian.go
+++ ginCase5/mian.go
@@ -5,13 +5,18 @@ import (
 	"golang_Gin/ginCase5/routers"
 	"html/template"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	//Default默认使用了Logger，Recover中间件，如果不想使用可以换做gin.New()
 	r := gin.Default()
-
+	//配置session中间件，创建一个基于cookie的存储引擎，secret是加密密钥
+	store := cookie.NewStore([]byte("secret"))
+	//store是存储引擎，可以替换
+	r.Use(sessions.Sessions("mysession", store))
 	//在中间件或者handler中启动新的goroutine时，不能使用原始的上下文（c *gin.Context）,必须使用其只读副本c.Copy()
 	r.SetFuncMap(template.FuncMap{
 		"UnixToTime": models.UnixToTime,
