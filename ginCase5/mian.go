6263702ac35f49c32962c0dcaad04af56d2e7057
JonesAshbur <jonesashbur@gmail.com>
2025-03-25 12:32:28 +0800

    update

--- ginCase5/mian.go
+++ ginCase5/mian.go
@@ -27,7 +27,7 @@ func main() {
 	routers.AdminRoutersInit(r)
 	routers.ApiRoutersInit(r)
 
-	//80端口访问时直接访问localhost，无需加端口号
+	//设置为80端口访问时直接访问localhost，无需加端口号
 	err := r.Run(":80")
 	if err != nil {
 		return
